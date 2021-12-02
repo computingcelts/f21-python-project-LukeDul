2eba9d03a268dc478f18093b74ad87cb563303e6
Luke Dulworth <lukedulworth2@gmail.com>
2021-12-02 16:37:26 -0600

    plot_top_k_service_types finished

--- cohstats/graphics/graphics.py
+++ cohstats/graphics/graphics.py
@@ -40,7 +40,6 @@ def draw_bar(x, y, height, width, color):
 def draw_bars(x, y, color, width=10, gap=5,  total_bars=1):
     i = 0
     while i < total_bars :
-        draw_text('Click anywhere on the window to close turtle graphics', 10, 10)
         draw_bar(x + (i*(width + gap)), 50, 400, width, color)
         i += 1
 
@@ -48,11 +47,11 @@ def draw_bars(x, y, color, width=10, gap=5,  total_bars=1):
 def render() :
     turtle.update()  # Render image
     turtle.exitonclick()  # Wait for user's mouse click
-
-
+#
+# def draw_bar(x, y, height, width, color):
 # You have to implement the following 4 functions
 def plot_top_k_service_types(data, metadata, k):
-    draw_text('Displays graph: plot_top_k_service_types', 10, 520)
+    draw_text('Displays graph: plot_top_k_service_types', 10, 650)
     unsorted_list = []
     # print('unsorted', unsorted_list)
 
@@ -68,19 +67,25 @@ def plot_top_k_service_types(data, metadata, k):
         if index < k:
             final_list.append(i)
         index += 1
-    # print(final_list)
+    print(final_list)
+    bars = 0
+    index_2 = 0
+    distance = 20
+    while bars < k:
+        draw_bar(distance, 550, final_list[index_2], 5, 'green')
+        index_2 += 1
+        bars += 1
+        distance += 20
 
 
-
-
-def plot_bottom_k_service_types(data, metadata, k):
-    draw_text('Displays graph: plot_bottom_k_service_types', 10, 500)
-
-
-def plot_service_by_day(data, day):
-    draw_text('Displays graph: plot_service_by_day', 10, 480)
-
-
-def plot_service_by_hour(data, hour):
-    draw_text('Displays graph: plot_service_by_hour', 10, 460)
+# def plot_bottom_k_service_types(data, metadata, k):
+#     draw_text('Displays graph: plot_bottom_k_service_types', 10, 500)
+#
+#
+# def plot_service_by_day(data, day):
+#     # draw_text('Displays graph: plot_service_by_day', 10, 480)
+#
+#
+# def plot_service_by_hour(data, hour):
+#     # draw_text('Displays graph: plot_service_by_hour', 10, 460)
 
